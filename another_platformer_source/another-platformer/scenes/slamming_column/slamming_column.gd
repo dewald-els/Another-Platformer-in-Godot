c17031fd108cc261f4294b07d513690e4d86eb95
Dewald Els <dewaldifels@gmail.com>
2024-10-08 23:09:09 +0200

    feat :Column groups with patterns

--- another_platformer_source/another-platformer/scenes/slamming_column/slamming_column.gd
+++ another_platformer_source/another-platformer/scenes/slamming_column/slamming_column.gd
@@ -1,54 +1,51 @@
 class_name SlammingColumn
 extends StaticBody2D
 
-@onready var delay_timer: Timer = %DelayTimer
-@onready var withdraw_delay_timer: Timer = %WithdrawDelayTimer
-@onready var damage_area_collision: CollisionShape2D = %DamageAreaCollision
+signal withdraw_complete
+signal slam_complete
 
+@onready var damage_area_collision: CollisionShape2D = %DamageAreaCollision
 
 const COLUMN_HEIGHT: int = 49 # + 1 Pixel for hiding behind Platforms
 
-@export var start_delay: float = 0.01
-@export var withdraw_duration: float = 4.0
-@export var withdraw_delay: float = 0.01
-@export var slam_speed: float = 0.25
+var is_withdrawing: bool = false
+var withdraw_duration: float = 4.0
+var slam_speed: float = 0.25
+
 @export_enum("UP", "DOWN") var direction = "DOWN"
 
 var start_position: Vector2 = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	start_position = global_position
 	
-	delay_timer.wait_time = start_delay
-	delay_timer.timeout.connect(_handle_delay_timer_timeout)
 	
-	withdraw_delay_timer.wait_time = withdraw_delay
-	withdraw_delay_timer.timeout.connect(_handle_withdraw_delay_timeout)
-	
-	delay_timer.start()
-	
-	
-func _handle_delay_timer_timeout() -> void:
-	_slam()
-	
-func _handle_withdraw_delay_timeout() -> void:
+func withdraw() -> void:
+	is_withdrawing = true
 	damage_area_collision.disabled = true
 	var tween: Tween = create_tween()
 	tween.tween_property(self, "global_position", start_position, withdraw_duration)\
 		.set_ease(Tween.EASE_OUT)\
 		.set_trans(Tween.TRANS_CUBIC)
-		
-	tween.tween_callback(_restart_timer)
+	
+	tween.tween_callback(_handle_withdraw_complete)
 
-func _slam() -> void:
+func slam() -> void:
+	if is_withdrawing:
+		return
+		
 	damage_area_collision.disabled = false
 	var tween: Tween = create_tween()
 	tween.tween_property(self, "global_position", global_position + (Vector2.DOWN * COLUMN_HEIGHT), slam_speed)\
 		.set_ease(Tween.EASE_OUT)\
 		.set_trans(Tween.TRANS_CUBIC)
-		
-	withdraw_delay_timer.start()
 	
-func _restart_timer() -> void:
-	delay_timer.start()
+	tween.tween_callback(_handle_slam_complete)
+	
+	
+func _handle_slam_complete() -> void:
+	slam_complete.emit()
+
+func _handle_withdraw_complete() -> void:
+	is_withdrawing = false
+	withdraw_complete.emit()
---
+++ another_platformer_source/another-platformer/scenes/slamming_column/slamming_column_group.gd
@@ -0,0 +1,91 @@
+class_name SlammingColumnGroup
+extends Node2D
+
+@onready var start_delay_timer: Timer = %StartDelayTimer
+
+@export var start_delay_time: float = 0.0
+
+@export var withdraw_duration: float = 4.0
+@export var withdraw_delay: float = 0.0
+@export var slam_speed: float = 0.25
+@export_enum("Wait", "OnSlam", "Together") var slam_pattern = "Wait"
+
+var columns: Array[SlammingColumn] = []
+var current_column_index: int = 0
+var total_withdrawn: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	_find_columns_in_group()
+	
+	if start_delay_time == 0.0:
+		start_delay_time = randf_range(0.1, 1.0)
+		
+	start_delay_timer.wait_time = start_delay_time
+	start_delay_timer.timeout.connect(_handle_delay_timer_timeout)
+	start_delay_timer.start()
+	
+func _find_columns_in_group() -> void:
+	for child in get_children():
+		if "SlammingColumn" in child.name:
+			columns.append(child as SlammingColumn)
+			
+	var column_index: int = 0
+	for column in columns:
+		column.withdraw_duration = withdraw_duration
+		column.slam_speed = slam_speed
+		column.slam_complete.connect(func(): _handle_slam_complete(column_index))
+		column.withdraw_complete.connect(func(): _handle_withdraw_complete(column_index))
+		column_index += 1
+
+
+func _handle_withdraw_complete(index: int) -> void:
+	if slam_pattern == "Wait":
+		_slam_next_column()
+	
+	total_withdrawn += 1
+	
+	if slam_pattern == "Together" and total_withdrawn == columns.size():
+		total_withdrawn = 0
+		_slam_all_columns()
+
+
+func _handle_slam_complete(index: int) -> void:
+	
+	columns[index].withdraw()
+	
+	if slam_pattern == "OnSlam":
+		_slam_next_column()
+
+
+# Can't get the withdraw delay to work as expected.
+#func _withdraw_column(index: int) -> void:
+	#if withdraw_delay > 0.0:
+		#await get_tree().create_timer(withdraw_delay).timeout
+		#columns[index].withdraw()
+	#else:
+		#columns[index].withdraw()
+
+func _slam_next_column() -> void:
+	var column = columns[current_column_index]
+	if not column.is_withdrawing:
+		column.slam()
+		current_column_index += 1
+		
+		if current_column_index == columns.size():
+			current_column_index = 0
+
+func _slam_all_columns() -> void:
+	for column in columns:
+		column.slam()
+
+func _handle_delay_timer_timeout() -> void:
+	if columns.size() <= 0:
+		return
+
+	if slam_pattern == "Together":
+		_slam_all_columns()
+	else:
+		_slam_next_column()
+	
