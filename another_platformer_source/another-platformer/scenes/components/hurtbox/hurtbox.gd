8f39c037c7d6bdb961a8379221be051f62fcd2c9
Dewald Els <dewaldifels@gmail.com>
2024-10-09 17:26:36 +0200

    feat: Level 8, bug fixes and spike tile layers

--- another_platformer_source/another-platformer/scenes/components/hurtbox/hurtbox.gd
+++ another_platformer_source/another-platformer/scenes/components/hurtbox/hurtbox.gd
@@ -7,7 +7,13 @@ extends Area2D
 
 func _ready() -> void:
 	area_entered.connect(_handle_area_entered)
+	body_entered.connect(_handle_body_entered)
 
+
+func _handle_body_entered(_other_body: Node2D) -> void:
+	Logger.create("Hurtbox._handle_body_entered", "Some area entered the hurtbox" + _other_body.name)
+	if health:
+		health.damage(1)
 
 func _handle_area_entered(_other_body: Area2D) -> void:
 	Logger.create("Hurtbox._handle_area_entered", "Some area entered the hurtbox")
