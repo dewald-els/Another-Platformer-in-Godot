1c4bc040dc4d919a68d4e587bf30e45f1a76ad21
Dewald Els <dewaldifels@gmail.com>
2024-10-11 20:48:13 +0200

    feat: Spike cannons, Level 9

--- another_platformer_source/another-platformer/scenes/spike_cannon/spike_cannon.gd
+++ another_platformer_source/another-platformer/scenes/spike_cannon/spike_cannon.gd
@@ -2,23 +2,31 @@ class_name SpikeCannon
 extends Node2D
 
 @onready var shoot_timer: Timer = %ShootTimer
+@onready var bullet_origin: Marker2D = %BulletOrigin
 
 @export_enum("Left", "Right") var shoot_direction = "Right"
 @export var bullet_scene: PackedScene
 @export var shoot_interval: float = 1.0
 @export var bullet_speed: float = 100.0
+@export var destroy_bullet_after: float = 1.0
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	shoot_timer.wait_time = shoot_interval
 	shoot_timer.timeout.connect(_handle_shoot_timer_timeout)
+	shoot_timer.start()
+	
 	
 func _shoot() -> void:
+	Logger.create("Cannon._shoot", "Shooting")
 	var bullet_instance: SpikeCannonBullet = bullet_scene.instantiate()
 	var projectiles_container = get_tree().get_first_node_in_group("projectiles")
 	bullet_instance.direction = _direction_to_int()
 	bullet_instance.speed = bullet_speed
+	bullet_instance.destroy_wait_time = destroy_bullet_after
 	projectiles_container.add_child(bullet_instance)
+	bullet_instance.global_position = bullet_origin.global_position
+	shoot_timer.start()
 	
 func _direction_to_int() -> int:
 	if shoot_direction == "Left":
--- another_platformer_source/another-platformer/scenes/spike_cannon/spike_cannon_bullet/spike_cannon_bullet.gd
+++ another_platformer_source/another-platformer/scenes/spike_cannon/spike_cannon_bullet/spike_cannon_bullet.gd
@@ -1,9 +1,25 @@
 class_name SpikeCannonBullet
-extends CharacterBody2D
+extends Node2D
+
+@onready var destroy_timer: Timer = %DestroyTimer
+@onready var sprite_2d: Sprite2D = %Sprite2D
 
 var speed: float = 5.0
 var direction: int = 0
+var destroy_wait_time: float = 1.0
 
+func _ready() -> void:
+	Logger.create("Bullet", "direction: " + str(direction))
+	Logger.create("Bullet", "speed: " + str(speed))
+	destroy_timer.timeout.connect(_handle_destroy_timeout)
+	destroy_timer.wait_time = destroy_wait_time
+	destroy_timer.start()
+	scale.x = direction
+		
 
 func _physics_process(delta: float) -> void:
-	velocity.x = speed * direction * delta
+	position.x += speed * direction * delta
+
+
+func _handle_destroy_timeout() -> void:
+	call_deferred("queue_free")
